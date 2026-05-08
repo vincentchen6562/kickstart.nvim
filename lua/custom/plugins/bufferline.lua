33bbcefb5f90bc8c21633e02d7bb6a7211a5aa77
Vincent Chen <vch6562@gmail.com>
2026-05-09 00:21:54 +1200

    Added keymaps to bufferline

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -1,5 +1,13 @@
 vim.pack.add {'https://github.com/akinsho/bufferline.nvim'}
 vim.opt.termguicolors = true
+
+-- Custom key maps for tab
+vim.keymap.set("n", "<Tab>", "<cmd>BufferLineCycleNext<cr>")
+vim.keymap.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<cr>")
+vim.keymap.set("n", "<C-x>", "<cmd>bdelete<cr>")  -- close buffer
+vim.keymap.set("n", "<leader>bp", "<cmd>BufferLinePick<cr>", { silent = true })
+vim.keymap.set("n", "<leader>bx", "<cmd>BufferLinePickClose<cr>", { silent = true })
+
 require("bufferline").setup({
 	options= {
 		color_icons = true,
